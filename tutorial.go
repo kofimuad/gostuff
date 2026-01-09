9338f208effa48146bf105ed2d71559b935bada9
kofimuad <iamkofiagyei@gmail.com>
2026-01-09 12:17:46 +0000

    varibles and all

--- tutorial.go
+++ tutorial.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Welcome to the quiz game!")
+
+	name := "Tim"
+	age := 23
+
+	fmt.Printf("Hello, %v! How are you doing today. You are %v years old", name, age)
 }
