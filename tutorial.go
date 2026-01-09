55efa36f830b24b9c6cdae70b9015cf353eaa54c
kofimuad <iamkofiagyei@gmail.com>
2026-01-09 12:44:23 +0000

    game started

--- tutorial.go
+++ tutorial.go
@@ -5,8 +5,32 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to the quiz game!")
 
-	name := "Tim"
-	age := 23
+	fmt.Println("Enter your name pls:")
+	var name string
+	fmt.Scan(&name)
 
-	fmt.Printf("Hello, %v! How are you doing today. You are %v years old", name, age)
+	fmt.Println("Enter your age pls:")
+	var age uint
+	fmt.Scan(&age)
+
+	if age >= 10 {
+		fmt.Printf("You're welcome %v, you are %v years old and eligible for the game\n", name, age)
+	} else {
+		fmt.Printf("You're welcome %v, you are %v years old hence you're not eligible for the game\n", name, age)
+		return
+	}
+
+	fmt.Printf("What is the name of the first president of the republic of Ghana: ")
+	var answer string
+	var answer2 string
+	fmt.Scan(&answer, &answer2)
+
+	switch answer + " " + answer2 {
+	case "Kwame Nkrumah":
+		fmt.Printf("You're correct!")
+	case "kwame nkrumah":
+		fmt.Printf("You're correct!")
+	default:
+		fmt.Printf("Try again, you're wrong")
+	}
 }
